686301ee54f4fecdee528e35198273b004f2439d
Leonardo <leondani853@gmail.com>
2025-01-24 15:22:04 -0300

    doc: add swagger

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -4,6 +4,8 @@ import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,18 +17,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/games")
+@Tag(name = "Games")
 public class GameController {
 
     @Autowired
     private GameService gameService;
 
     @GetMapping
+    @Operation(summary = "List all games", description = "Return a list off all games")
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(summary = "List game by id", description = "Return a unique game by id")
     public GameDTO findById(@PathVariable Long id) {
         GameDTO result = gameService.findById(id);
         return result;
--- src/main/java/com/devsuperior/dslist/controller/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameListController.java
@@ -5,6 +5,8 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/lists")
+@Tag(name = "List game")
 public class GameListController {
 
     @Autowired
@@ -23,12 +26,14 @@ public class GameListController {
     private GameService gameService;
 
     @GetMapping
+    @Operation(summary = "List all type of game", description = "Return a list contain a name of the games")
     public List<GameListDTO> findAll() {
         List<GameListDTO> result = gameListService.findAll();
         return result;
     }
 
     @GetMapping(value = "/{listId}/games")
+    @Operation(summary = "List a type of a game by id", description = "Return a specific type of game by id")
     public List<GameMinDTO> findAll(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
